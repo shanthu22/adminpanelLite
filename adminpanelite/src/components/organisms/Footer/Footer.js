ebe97d3a77a161fd141013a31cab9c5fd275bbef
Shanthu Shanthu <ssanthujan@yahoo.com>
2024-04-27 22:45:31 +0530

    manage prd

--- adminpanelite/src/components/organisms/Footer/Footer.js
+++ adminpanelite/src/components/organisms/Footer/Footer.js
@@ -1,7 +1,24 @@
 import "./Footer.css";
 
 const Footer = () => {
-  return <div className="Footer"></div>;
+  return (
+    <div className="Footer">
+      <div className="FooterRight">
+        &copy; {new Date().getFullYear()}, made with ❤️ by
+        <a
+          href="https://tekkzen.com/"
+          target="_blank"
+          rel="noopener noreferrer">
+          Tekkzen
+        </a>
+      </div>
+      <div className="FooterLeft">
+        <div className="FooterLeftBtn"> Tekkzen</div>
+        <div className="FooterLeftBtn">About Us</div>
+        <div className="FooterLeftBtn">Contact Us</div>
+      </div>
+    </div>
+  );
 };
 
 export default Footer;
