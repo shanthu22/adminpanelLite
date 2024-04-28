b91b9b4e5f8c1976a590b62c15e166048ef7608b
Shanthu Shanthu <ssanthujan@yahoo.com>
2024-04-28 06:13:03 +0530

    api

--- adminpanelite/src/components/templates/MainLayout/MainLayout.js
+++ adminpanelite/src/components/templates/MainLayout/MainLayout.js
@@ -1,9 +1,10 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./MainLayout.css";
 import Navbar from "../../organisms/Navbar/Navbar";
 import ManageProduct from "../../pages/ManageProduct/ManageProduct";
 import Footer from "../../organisms/Footer/Footer";
 import ProductDisplay from "../../molecules/ProductDisplay/ProductDisplay";
+import { apiFetch } from "../../../utils/api/Api";
 const MainLayout = () => {
   const TableName = "Default Table Name";
   const TableHeaders = [
@@ -16,81 +17,16 @@ const MainLayout = () => {
     "Image Path",
     "Action",
   ];
-  const TableData = [
-    {
-      id: 1,
-      name: "Default Name",
-      description: "Default Description",
-      price: "0.00",
-      quantity: 0,
-      expDate: "2024-12-31",
-      imagePath: "default.jpg",
-    },
-    {
-      id: 2,
-      name: "Apple",
-      description: "Ripe banana with potassium",
-      price: "0.49",
-      quantity: 150,
-      expDate: "2024-12-29T00:00:00.000Z",
-      imagePath: "apple.jpeg",
-    },
-    {
-      id: 3,
-      name: "Chicken Breast",
-      description: "Boneless skinless chicken breast",
-      price: "4.99",
-      quantity: 50,
-      expDate: "2024-12-31T00:00:00.000Z",
-      imagePath: "chicken.jpg",
-    },
-    {
-      id: 4,
-      name: "Baby Onesie",
-      description: "Soft cotton onesie for infants",
-      price: "12.99",
-      quantity: 50,
-      expDate: "2024-12-31T00:00:00.000Z",
-      imagePath: "onesie.jpg",
-    },
-    {
-      id: 1,
-      name: "Default Name",
-      description: "Default Description",
-      price: "0.00",
-      quantity: 0,
-      expDate: "2024-12-31",
-      imagePath: "default.jpg",
-    },
-    {
-      id: 2,
-      name: "Apple",
-      description: "Ripe banana with potassium",
-      price: "0.49",
-      quantity: 150,
-      expDate: "2024-12-29T00:00:00.000Z",
-      imagePath: "apple.jpeg",
-    },
-    {
-      id: 3,
-      name: "Chicken Breast",
-      description: "Boneless skinless chicken breast",
-      price: "4.99",
-      quantity: 50,
-      expDate: "2024-12-31T00:00:00.000Z",
-      imagePath: "chicken.jpg",
-    },
-    {
-      id: 4,
-      name: "Baby Onesie",
-      description: "Soft cotton onesie for infants",
-      price: "12.99",
-      quantity: 50,
-      expDate: "2024-12-31T00:00:00.000Z",
-      imagePath: "onesie.jpg",
-    },
-  ];
+  const [TableData, setTableData] = useState();
 
+  const FetchData = async (TableName) => {
+    const response = await apiFetch(TableName);
+    console.log(response.data[0]);
+    setTableData(response.data[0]);
+  };
+  useEffect(() => {
+    FetchData("food");
+  }, []);
   return (
     <div className="MainLayout">
       <Navbar />
