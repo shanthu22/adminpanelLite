12873aaeb7a27e469d0cfd3ffdc661a1c14b9a8f
Shanthu Shanthu <ssanthujan@yahoo.com>
2024-05-12 03:02:53 +0530

    image upload

--- adminpanelite/src/components/molecules/AddInputForm/AddInputForm.js
+++ adminpanelite/src/components/molecules/AddInputForm/AddInputForm.js
@@ -1,6 +1,7 @@
 import Popup from "../../atoms/Popup/Popup";
 import { useState } from "react";
 import "./AddInputForm.css";
+import FileUpload from "../../atoms/FileUpload/FileUpload";
 const AddInputForm = ({ HandleCRUD }) => {
   const [Formdata, setFormdata] = useState([]);
   const HandleOnchange = (e) => {
@@ -16,78 +17,85 @@ const AddInputForm = ({ HandleCRUD }) => {
     <Popup>
       <div className="AddInputForm">
         <div>Add Product </div>
-        <div className="Image"></div>
-        <div className="form">
-          <label>
-            <input
-              placeholder="Name"
-              name="name"
-              onChange={(e) => HandleOnchange(e)}
-              value={Formdata.name}
-              type="text"
-              className="input"
-            />
-          </label>
-          <label>
-            <input
-              required=""
-              placeholder="Description"
-              name="description"
-              onChange={(e) => HandleOnchange(e)}
-              value={Formdata.description}
-              type="text"
-              className="input"
-            />
-          </label>
-          <label>
-            <input
-              required=""
-              placeholder="Price"
-              name="price"
-              onChange={(e) => HandleOnchange(e)}
-              value={Formdata.price}
-              type="text"
-              className="input"
-            />
-          </label>
-          <label>
-            <input
-              required=""
-              placeholder="Quantity"
-              name="quantity"
-              onChange={(e) => HandleOnchange(e)}
-              value={Formdata.quantity}
-              type="text"
-              className="input"
-            />
-          </label>
-          <label>
-            <input
-              required=""
-              placeholder="Expiray Date"
-              name="expDate"
-              onChange={(e) => HandleOnchange(e)}
-              value={Formdata.expDate}
-              type="text"
-              className="input"
-            />
-          </label>
-          <label>
-            <input
-              required=""
-              placeholder="Image Path"
-              name="imagePath"
-              onChange={(e) => HandleOnchange(e)}
-              value={Formdata.imagePath}
-              type="text"
-              className="input"
-            />
-          </label>
-          <label>
-            <button className="submitBtn" onClick={(e) => HandleSubmit(e)}>
-              Submit
-            </button>
-          </label>
+
+        <div className="Addform">
+          <div className="AddFormLeftSide">
+            <div className="AddImage">
+              <FileUpload />
+            </div>
+          </div>
+          <div className="AddFormRightSide">
+            <label>
+              <input
+                placeholder="Name"
+                name="name"
+                onChange={(e) => HandleOnchange(e)}
+                value={Formdata.name}
+                type="text"
+                className="input"
+              />
+            </label>
+            <label>
+              <input
+                required=""
+                placeholder="Description"
+                name="description"
+                onChange={(e) => HandleOnchange(e)}
+                value={Formdata.description}
+                type="text"
+                className="input"
+              />
+            </label>
+            <label>
+              <input
+                required=""
+                placeholder="Price"
+                name="price"
+                onChange={(e) => HandleOnchange(e)}
+                value={Formdata.price}
+                type="text"
+                className="input"
+              />
+            </label>
+            <label>
+              <input
+                required=""
+                placeholder="Quantity"
+                name="quantity"
+                onChange={(e) => HandleOnchange(e)}
+                value={Formdata.quantity}
+                type="text"
+                className="input"
+              />
+            </label>
+            <label>
+              <input
+                required=""
+                placeholder="Expiray Date"
+                name="expDate"
+                onChange={(e) => HandleOnchange(e)}
+                value={Formdata.expDate}
+                type="text"
+                className="input"
+              />
+            </label>
+            <label>
+              <input
+                required=""
+                placeholder="Image Path"
+                name="imagePath"
+                onChange={(e) => HandleOnchange(e)}
+                value={Formdata.imagePath}
+                type="text"
+                className="input"
+              />
+            </label>
+            <label>
+              <button className="submitBtn" onClick={(e) => HandleSubmit(e)}>
+                Submit
+              </button>
+            </label>
+          </div>
         </div>
       </div>
     </Popup>
